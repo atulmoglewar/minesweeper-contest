94aec41549a8a2a7afcf0043058f34d4f6152a30
Atul Moglewar <atulmoglewar@gmail.com>
2017-07-24 13:36:19 +0530

    Moved client code inside client folder.

---
+++ client/webpack.config.js
@@ -0,0 +1,23 @@
+module.exports  = {
+  entry: './src/js/main.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /.jsx?/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true
+  }
+}
