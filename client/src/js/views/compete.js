dbcf40d9884613e2a7f347a3a0e5874488e2f130
Atul Moglewar <atulmoglewar@gmail.com>
2017-07-25 15:57:33 +0530

    Implemented competetion page.

--- client/src/js/views/compete.js
+++ client/src/js/views/compete.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import MineSweeper from './mineSweeper.js'
+import BG from './bg.js'
+import GameScreen from './gameScreen';
+import * as MinesweeperActions from '../data/actions/minesweeperActions.js';
+import * as Config from '../data/config/level.js';
+import * as Page from '../data/config/routes.js';
+import Header from './header'
 
 export default class Compete extends React.Component {
-  
+  componentWillMount() {
+    MinesweeperActions.levelChange("advance", Config.Levels.advance);
+  }
   render() {
-    return <div>Compete Page</div>
+    let props = [Page.Routes.home, Page.Routes.practice, 
+    Page.Routes.leaderboard, Page.Routes.help];
+    return (
+      <div>
+        <BG transparent="true"/>
+        <Header links={props}/>
+        <GameScreen />
+      </div>
+    )
   }
 }
