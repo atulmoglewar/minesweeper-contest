add483def872202812bd32f241f1f63cfaad950f
Prasanna Barate <barateprasanna@gmail.com>
2017-08-11 18:04:35 +0530

    Added profile pic to user profile.
    1. Fetching profile pic from google, facebook, twitter after successful
       login :
          1.1. Added "SOCIAL_AUTH_PIPELINE" in settings.py file.
          1.2. Added "pipeline.py" file in application which actually
               has implementation to get profile pic and save it in local
               repo.
    2. Create new database model "UserProfile" to store user related data
       like profile_pic, gender etc.
       2.1. For now we just save profile pic in UserProfile in ImageField
           against current user.
    3. In "profiles" repo, we will save profile pics for all users.
       3.1. currently, we saves profile images in .jpg format.
       3.2. file name format is "username" + "social" + "extension"

--- server/MineSweeper_Project/settings.py
+++ server/MineSweeper_Project/settings.py
@@ -83,6 +83,20 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 )
 
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+    # This is a path of your pipeline.py and get_avatar is the function.
+    'app.pipeline.get_avatar', 
+)
+
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
--- server/app/models.py
+++ server/app/models.py
@@ -6,3 +6,10 @@ class WinRecord(models.Model):
 	user = models.ForeignKey(User, help_text= "User to whom record is belogs", null=True, on_delete=models.CASCADE)
 	time = models.FloatField(help_text="Time taken to sweep all mines")
 	level = models.CharField(max_length= 20, help_text="Level could be either Advanced or Nightmare")
+
+class UserProfile(models.Model):
+	user = models.OneToOneField(User)
+	profile_photo = models.ImageField(upload_to='profiles', null=True)
+	
+	def __str__(self):
+		return "%s's profile" % self.user
